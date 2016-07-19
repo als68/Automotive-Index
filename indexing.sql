44715e54d14c0dc56db6d6494e912d2cf816c8b9
Andy <suhandyl@gmail.com>
2016-07-19 10:06:09 -1000

    First 3 queries completed

---
+++ indexing.sql
@@ -0,0 +1,32 @@
+/*Make a note of the time of each query with \timing*/
+
+/*SELECT DISTINCT make_title FROM car_models WHERE make_code = 'LAM';*/
+
+/*SELECT DISTINCT model_title FROM car_models WHERE make_code = 'NISSAN' AND model_code = 'GT-R';*/
+
+/*SELECT make_code, model_code, model_title, year FROM car_models WHERE make_code = 'LAM';*/
+
+/*SELECT * FROM car_models WHERE year > 2009 AND year < 2016;*/
+
+/*SELECT * FROM car_models WHERE year = 2010;*/
+
+\timing
+
+--1
+SELECT DISTINCT make_title
+  FROM car_models
+  WHERE make_code = 'LAM';
+
+--2
+SELECT DISTINCT model_title
+  FROM car_models
+  WHERE make_code = 'NISSAN' AND model_code = 'GT-R';
+
+--3
+SELECT make_code, model_code, model_title, year
+  FROM car_models
+  WHERE make_code = 'LAM';
+
+--4
+
+--5
